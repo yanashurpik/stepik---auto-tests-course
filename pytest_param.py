727b4282859599b81c165bf68be07fa9e7b59dd7
yanashurpik <yana_shurpik@imap.by>
2020-05-06 00:03:12 +0300

    my pytest training

---
+++ pytest_param.py
@@ -0,0 +1,47 @@
+import pytest
+from selenium import webdriver
+
+links = ['https://stepik.org/lesson/236895/step/1',
+'https://stepik.org/lesson/236896/step/1',
+'https://stepik.org/lesson/236897/step/1',
+'https://stepik.org/lesson/236898/step/1',
+'https://stepik.org/lesson/236899/step/1',
+'https://stepik.org/lesson/236903/step/1',
+'https://stepik.org/lesson/236905/step/1']
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('page', links)
+def test_web(browser, page):
+    browser.implicitly_wait(10)
+    import time
+    import math
+    answer = str(math.log(int(time.time())))
+    final = ''
+
+    browser.get(page)
+    input1 = browser.find_element_by_css_selector("#ember70")
+    input1.send_keys(answer)
+
+    button = browser.find_element_by_css_selector(".submit-submission")
+    button.click()
+
+
+    respons_sel = browser.find_element_by_css_selector(".smart-hints__hint")
+    response = respons_sel.text
+
+    try:
+        assert response == "Correct!", "Result is not correct"
+    except AssertionError:
+        final += response
+
+
+
+
